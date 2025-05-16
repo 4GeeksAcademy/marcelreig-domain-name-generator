5373d218fadec2cac7c70972fe15138397519cf7
Marcel Reig <marcelreig@gmail.com>
2025-05-16 18:11:30 +0000

    add domain geneartor function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+window.onload = function () {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ['jogger', 'racoon'];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let i = 0; i < adj.length; i++) {
+       for (let i = 0; i < noun.length; i++) {
+        let dominio = pronoun[i] + adj[i] + adj[i] + ".com";
+         console.log(dominio);
+       }
+    }
+  }
+
+   console.log(dominio);
+
+
 };
